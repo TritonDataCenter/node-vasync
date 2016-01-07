c9cf3bad562eb58ac680ee18a0d7c41950fd6227
Dave Pacheco <dap@joyent.com>
2016-01-07 09:30:02 -0800

    add timing information

--- examples/pipeline.js
+++ examples/pipeline.js
@@ -3,10 +3,11 @@ var mod_util = require('util');
 var mod_vasync = require('../lib/vasync');
 
 console.log(mod_vasync.pipeline({
+    'trackTime': true,
     'funcs': [
-	function f1(_, callback) { mod_dns.resolve('joyent.com', callback); },
-	function f2(_, callback) { mod_dns.resolve('github.com', callback); },
-	function f3(_, callback) { mod_dns.resolve('asdfaqsd.com', callback); }
+	function f1(_, callback) { mod_dns.lookup('joyent.com', callback); },
+	function f2(_, callback) { mod_dns.lookup('github.com', callback); },
+	function f3(_, callback) { mod_dns.lookup('asdfaqsd.com', callback); }
     ]
 }, function (err, results) {
 	console.log('error: %s', err.message);
